8daad1611e3315be4e2e0fa274bc97f0e96740b0
Ahmed Maher <demha.ahmed@yahoo.com>
2019-05-10 01:28:38 +0200

    FC multiplication modified

--- VHDL/FC/booth_adder_components.vhd
+++ VHDL/FC/booth_adder_components.vhd
@@ -65,69 +65,89 @@ signal temp_cout_8: std_logic;
 signal temp_cout_9: std_logic;
 -------------------------------------------------------------------
 signal booth_integration_output_0 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_0_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_1 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_1_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_2 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_2_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_3 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_3_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_4 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_4_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_5 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_5_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_6 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_6_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_7 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_7_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_8 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_8_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 signal booth_integration_output_9 : std_logic_vector (2*n-1 downto 0);
+signal booth_out_9_shifted :std_logic_vector (15 downto 0);
 
 -----------------------------------------------------------------------------------------------------------------------------------------
 begin
 shift_Reg_adder_0:entity work.shift_reg_adder  generic map (n+4) port map ( set, clk,shift_reg_output);
 ------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_0:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_0,booth_integration_output_0);
-adder_0:entity work.nadder  generic map (n) port map (booth_integration_output_0(15 downto 0),  adderInput_0 , '0' ,label_0,temp_cout_0);
+booth_out_0_shifted <= booth_integration_output_0(27 downto 12);
+adder_0:entity work.nadder  generic map (n) port map (booth_out_0_shifted,  adderInput_0 , '0' ,label_0,temp_cout_0);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_1:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_1,booth_integration_output_1);
-adder_1:entity work.nadder  generic map (n) port map (booth_integration_output_1(15 downto 0),  adderInput_1 , '0' ,label_1,temp_cout_1);
+booth_out_1_shifted <= booth_integration_output_1(27 downto 12);
+adder_1:entity work.nadder  generic map (n) port map (booth_out_1_shifted,  adderInput_1 , '0' ,label_1,temp_cout_1);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_2:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_2,booth_integration_output_2);
-adder_2:entity work.nadder  generic map (n) port map (booth_integration_output_2(15 downto 0),  adderInput_2 , '0' ,label_2,temp_cout_2);
+booth_out_2_shifted <= booth_integration_output_2(27 downto 12);
+adder_2:entity work.nadder  generic map (n) port map (booth_out_2_shifted,  adderInput_2 , '0' ,label_2,temp_cout_2);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_3:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_3,booth_integration_output_3);
-adder_3:entity work.nadder  generic map (n) port map (booth_integration_output_3(15 downto 0),  adderInput_3 , '0' ,label_3,temp_cout_3);
+booth_out_3_shifted <= booth_integration_output_3(27 downto 12);
+adder_3:entity work.nadder  generic map (n) port map (booth_out_3_shifted,  adderInput_3 , '0' ,label_3,temp_cout_3);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_4:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_4,booth_integration_output_4);
-adder_4:entity work.nadder  generic map (n) port map (booth_integration_output_4(15 downto 0),  adderInput_4 , '0' ,label_4,temp_cout_4);
+booth_out_4_shifted <= booth_integration_output_4(27 downto 12);
+adder_4:entity work.nadder  generic map (n) port map (booth_out_4_shifted,  adderInput_4 , '0' ,label_4,temp_cout_4);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_5:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_5,booth_integration_output_5);
-adder_5:entity work.nadder  generic map (n) port map (booth_integration_output_5(15 downto 0),  adderInput_5 , '0' ,label_5,temp_cout_5);
+booth_out_5_shifted <= booth_integration_output_5(27 downto 12);
+adder_5:entity work.nadder  generic map (n) port map (booth_out_5_shifted,  adderInput_5 , '0' ,label_5,temp_cout_5);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_6:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_6,booth_integration_output_6);
-adder_6:entity work.nadder  generic map (n) port map (booth_integration_output_6(15 downto 0),  adderInput_6 , '0' ,label_6,temp_cout_6);
+booth_out_6_shifted <= booth_integration_output_6(27 downto 12);
+adder_6:entity work.nadder  generic map (n) port map (booth_out_6_shifted,  adderInput_6 , '0' ,label_6,temp_cout_6);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_7:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_7,booth_integration_output_7);
-adder_7:entity work.nadder  generic map (n) port map (booth_integration_output_7(15 downto 0),  adderInput_7 , '0' ,label_7,temp_cout_7);
+booth_out_7_shifted <= booth_integration_output_7(27 downto 12);
+adder_7:entity work.nadder  generic map (n) port map (booth_out_7_shifted,  adderInput_7 , '0' ,label_7,temp_cout_7);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_8:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_8,booth_integration_output_8);
-adder_8:entity work.nadder  generic map (n) port map (booth_integration_output_8(15 downto 0),  adderInput_8 , '0' ,label_8,temp_cout_8);
+booth_out_8_shifted <= booth_integration_output_8(27 downto 12);
+adder_8:entity work.nadder  generic map (n) port map (booth_out_8_shifted,  adderInput_8 , '0' ,label_8,temp_cout_8);
 ----------------------------------------------------------------------------------------------------------------------------------------------
 booth_integrtaion_9:entity work.booth_integration  generic map (n)port map ( set, clk , rst ,neuron,weight_9,booth_integration_output_9);
-adder_9:entity work.nadder  generic map (n) port map (booth_integration_output_9(15 downto 0),  adderInput_9 , '0' ,label_9,temp_cout_9);
+booth_out_9_shifted <= booth_integration_output_9(27 downto 12);
+adder_9:entity work.nadder  generic map (n) port map (booth_out_9_shifted,  adderInput_9 , '0' ,label_9,temp_cout_9);
 ------------------------------------------------------------------------------------------------------------------------------------------
- dec_enable <= shift_reg_output(n+2);
+dec_enable <= shift_reg_output(n+2);
 dec_sel <= "1111" when shift_reg_output(n+2)='1'
 else  "0000" ;
 done <= shift_reg_output(n+3);
